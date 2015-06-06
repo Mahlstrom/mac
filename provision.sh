416cf086ff1706fd9ef4dcc046db64fb405c9ae6
Magnus Ahlstrom <magnus.ahlstrom@sbsradio.se>
2015-06-06 20:51:33 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,11 +3,16 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update
 # Install cask
 brew install caskroom/cask/brew-cask
+brew cask update
 
 # Install webstuff
 brew install php56 php56-xdebug composer nginx uwsgi postgresql node nginx
 
-brew cask update
+# Install other good stuff
+brew install watch
+
+
+# Install good programs to have
 brew cask install xquartz phpstorm sublime-text virtualbox vagrant istat-menus 
 
 # Installing sharing apps
