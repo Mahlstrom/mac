d94448ae9c37fc72375b471003f4b33608f43292
Magnus Ahlstrom <magnus.ahlstrom@bauermedia.se>
2015-10-27 15:00:15 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -7,15 +7,11 @@ brew update
 brew install caskroom/cask/brew-cask
 brew cask update
 
-# Install webstuff
-brew install php56 php56-xdebug composer nginx uwsgi postgresql node nginx
-
 # Install other good stuff
 brew install watch
 
-
 # Install good programs to have
-brew cask install xquartz phpstorm sublime-text virtualbox vagrant istat-menus 
+brew cask install xquartz phpstorm sublime-text virtualbox vagrant
 
 # Installing sharing apps
 brew cask install dropbox copy box-sync
