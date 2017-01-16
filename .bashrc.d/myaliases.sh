31808d1085d31b83604980d74ce1e2d957183395
Magnus Ahlström <magnus.ahlstrom@bauermedia.se>
2017-01-16 07:03:47 +0100

    Fixed some aliases

--- .bashrc.d/myaliases.sh
+++ .bashrc.d/myaliases.sh
@@ -1,4 +1,12 @@
 #Good to have
+if $(gls &>/dev/null)
+then
+  alias ls="gls -F --color"
+  alias l="gls -lAh --color"
+  alias ll="gls -l --color --time-style=long-iso"
+  alias la='gls -A --color'
+  alias lla='gls -lA --color --time-style=long-iso'
+fi
 alias ls='gls --color --time-style=long-iso'
 alias ll='gls -l --color --time-style=long-iso'
 alias gss='git status -s'
